b53f5cac351ca637e3d6b185a388329a87f639cb
Mui Kai En <muikaien1@gmail.com>
2017-09-03 12:31:06 +0800

    Add home route

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,11 @@ Vue.use(Router);
 export default new Router({
   routes: [
     {
+      path: '/',
+      name: 'Home',
+      component: Home,
+    },
+    {
       path: '/create',
       name: 'Create',
       component: Create,
