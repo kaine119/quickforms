5496c38cad6b806e01a7fc5043bc9fb16f58be2f
Mui Kai En <muikaien1@gmail.com>
2017-09-03 11:32:03 +0800

    Add a simple home screen
    The route `/` redirects to home screen. Two links: one to `/create`, one
    to `/{id}/`, depending on an <input>.

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="main">
+    <h3><router-link to="/create">Create a form</router-link></h3>
+    <br><br>
+    <input type="text" v-model="formID" placeholder="Form ID">
+    <h3><router-link :to="formLink">Find a form by ID</router-link></h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data: () => ({ formID: '' }),
+  computed: {
+    formLink() {
+      return `/${this.formID}`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
